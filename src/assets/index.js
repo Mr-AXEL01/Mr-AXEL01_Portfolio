2b0dd85cdd69ab5201466418effdac296744fc2f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-24 16:23:19 +0100

    add new logo and declare it on the index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import logo_white from "./logo_white.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -32,6 +33,7 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
+  logo_white,
   backend,
   creator,
   mobile,
