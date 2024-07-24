8b1ae14517b8497dd60f795faa809a4da5a5e99f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-24 03:22:24 +0100

    import our components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 function App() {
 
