c7823fe4ff47fd13a0945397d4fc1a859b99b8ac
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-24 20:08:23 +0100

    import the motion and styles and the model that we will use for our hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
