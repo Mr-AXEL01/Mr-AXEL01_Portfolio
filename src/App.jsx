1f4a4edbeeb9a9ba68d22494dff4dec5855f2dc7
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-24 03:28:36 +0100

    set our layout that we will follow on that project

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,23 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 function App() {
 
   return (
-    <div>
-      3D Developer portfolio
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
 
