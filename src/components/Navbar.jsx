1ced5b4ab0aac134799b3c3bab092292c146c248
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-24 18:43:40 +0100

    implement the list for mobile and set the span to be hidden on case of sm screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const[toggle, setToggle] = useState("false");
   return (
     <nav
       className="{`
@@ -24,7 +25,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
             ABD_ELHAQ AZROUR
-            <span>| Mr-AXEL01</span>
+            <span className="sm:block hidden">| Mr-AXEL01</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -40,6 +41,33 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img 
+            src={toggle ? close : menu} 
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 my-2 min-w-[140px] z-10 rounded-xl`}>
+          <ul className="list-none flex justify-end items-start flex-col gap-4">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } font-poppins font-medium curosr-pointer text-[16px]`}
+              onClick={() => {
+                setToggle(!toggle);
+                setActive(link.title)
+              }}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
