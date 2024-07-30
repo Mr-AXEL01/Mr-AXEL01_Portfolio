ffdd8d77a93aafd484a28095478f26646212a104
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-30 01:42:32 +0100

    update the descreption paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,13 +19,30 @@ const Hero = () => {
             </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100 `}>
-            Full Stack Developer specializing in web and mobile applications. 
+            Full Stack Developer 
             <br className="sm:block hidden"/>
-            Passionate about creating innovative solutions and improving user experiences.
+            Specializing in web and mobile applications.
           </p>
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
