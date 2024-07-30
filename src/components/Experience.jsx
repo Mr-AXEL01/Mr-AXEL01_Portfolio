7be7fa28da4b3ddcd651e78d8da6c5880b67d41e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-30 19:08:29 +0100

    set the company icons on the center of the border

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,7 +16,7 @@ const ExperienceCard = ({ experience }) => {
       date={experience.date}
       iconStyle={{ background: experience.iconBg}}
       icon={
-        <div>
+        <div className="flex justify-center items-center w-full h-full">
           <img src={experience.icon} alt={experience.company_name} 
             className="w-[60%] h-[60%] object-contain"
           />
