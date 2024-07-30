7122a3662b7203f9217ea326c8bcc1acdfa7bcfb
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-30 19:20:03 +0100

    restyle the companies name and map on the points or taches that we did on each company

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -27,12 +27,21 @@ const ExperienceCard = ({ experience }) => {
         <h3 className="text-white text-[24px] font-bold">
           {experience.title}
         </h3>
-        <p className="text-white text-[16px] font-medium">
+        <p className="text-secondary text-[16px] font-semibold"
+          style={{ margin: 0}}
+        >
           {experience.company_name}
         </p>
-        <p className="text-white text-[14px] mt-2">
-          {experience.description}
-        </p>
+        <ul className="mt-5 list-disc ml-5 space-y-2">
+          {experience.points.map((point, index) => (
+            <li
+              key={`experience-point-${index}`}
+              className="text-white-100 text-[14px] pl-1 tracking-wider"
+            >
+              {point}
+            </li>
+          ))}
+        </ul>
       </div>
     </VerticalTimelineElement>
   )
