2e0d88ba8db0a7cabf62cce52638a1a1b74e548f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-30 15:46:50 +0100

    done with the cards by setting the icons and titles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,12 @@ const ServiceCard = ({index, title, icon}) => {
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
-
+          <img src={icon} alt={title} 
+            className="w-16 h-16 object-contain"
+          />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
