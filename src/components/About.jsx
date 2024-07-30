ddb73af3a1b688520b543fc2190bffad328aefcb
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-30 16:02:31 +0100

    wrapper the about component with the SectionWrapper

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from "../hoc"
 
 const ServiceCard = ({index, title, icon}) => {
   return (
@@ -60,4 +61,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
