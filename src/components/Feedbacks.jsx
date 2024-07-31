f1bb7370d5d781f7010f5ba935993ac45dc20a5b
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-31 21:50:56 +0100

    import the necessary things that we will use on that components

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
+import { fadeIn, textVariant } from '../utils/motion';
+import { testminials } from '../constants';
 
 const Feedbacks = () => {
   return (
