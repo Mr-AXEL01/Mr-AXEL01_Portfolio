60e1ac9be8f7eb2e37c304254c50f983e2932c5f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-31 23:35:13 +0100

    update  the directionof import  for the canvas

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import emailjs from '@emailjs/browser';
 
 import { styles } from '../styles';
-import { EarthCanvas } from '../canvas';
+import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
