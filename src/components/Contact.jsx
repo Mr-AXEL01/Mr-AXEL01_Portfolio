44ad108976f6fba0cedacdc6110e3a1e349f30bb
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-31 23:31:02 +0100

    import the necessary thing that we need to use and set useState for the loading and an other for the form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+
+import { styles } from '../styles';
+import { EarthCanvas } from '../canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name:'',
+    email: '',
+    message: '',
+  })
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {}
+
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+
+    </div>
   )
 }
 
