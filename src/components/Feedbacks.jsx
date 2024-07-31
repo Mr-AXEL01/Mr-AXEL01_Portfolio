a67e3a9564321a156642e144fc0e61b298675953
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-07-31 22:01:21 +0100

    set a background to have the section we are wroking on and set the motion for our header

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -3,11 +3,22 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
-import { testminials } from '../constants';
+import { testimonials } from '../constants';
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className="mt-12 bg-black-100 rounded-[20px]">
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>
+            What others say
+          </p>
+          <h2 className={styles.sectionHeadText}>
+            Testimonails.
+          </h2>
+        </motion.div>
+      </div>
+    </div>
   )
 }
 
