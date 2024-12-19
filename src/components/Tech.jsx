3b30955efed90835aa1a13c93cab76311b475e24
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-19 22:58:01 +0100

    loop on all types of thecs that i work with.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,30 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
-import { technologies } from "../constants"; 
+import { backTtechnologies } from "../constants"; 
+import { frontendTechnologies } from "../constants"; 
+import { devopsAndToolsTechnologies } from "../constants"; 
 
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+      {backTtechnologies.map((backTechnology) => (
+        <div className="w-28 h-28" key={backTechnology.name}>
+          <BallCanvas icon={backTechnology.icon} />
         </div>
       ))}
+
+      {frontendTechnologies.map((frontTechnology) => (
+        <div className="w-28 h-28" key={frontTechnology.name}>
+          <BallCanvas icon={frontTechnology.icon} />
+        </div>
+      ))}
+
+      {devopsAndToolsTechnologies.map((devopsAndToolTechnology) => (
+        <div className="w-28 h-28" key={devopsAndToolTechnology.name}>
+          <BallCanvas icon={devopsAndToolTechnology.icon} />
+        </div>
+      ))}
+      
     </div>
   )
 }
